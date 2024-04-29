a9873c56c5234e63ca8c87080b08a1d27a4a6041
luoyi <luoyi58624@163.com>
2024-04-29 15:23:45 +0800

    delete example

--- example/luoyi_dart_test_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:luoyi_dart_test/luoyi_dart_test.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/luoyi_dart_test.dart
+++ lib/luoyi_dart_test.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/luoyi_dart_test_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+library luoyi_dart_test;
